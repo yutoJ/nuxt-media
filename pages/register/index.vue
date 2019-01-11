e8397aeebab199a9329b92caf6c3a760baacf585
yutoj <yutoogata4@gmail.com>
2019-01-11 10:27:34 +0900

    form validation

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -5,16 +5,22 @@
                 <div class="md-title">Register</div>
             </md-card-header>
 
-            <form @submit.prevent="registerUser">
+            <form @submit.prevent="validateForm">
                 <md-card-content>
-                    <md-field md-clearable>
+                    <md-field md-clearable :class="getValidationClass('email')">
                         <label for="email">Email</label>
                         <md-input :disabled="loading" type="email" id="email" autocomplete="email" v-model="form.email" />
+                        <span class="md-error" v-if="!$v.form.email.required">The email is required</span>
+                        <span class="md-error" v-else-if="!$v.form.email.emial">Invalid email</span>
                     </md-field>
 
-                    <md-field>
+                    <md-field :class="getValidationClass('password')">
                         <label for="passwprd">Password</label>
                         <md-input :disabled="loading" type="password" name="password" id="password" autocomplete="password" v-model="form.password" />
+                        <span class="md-error" v-if="!$v.form.password.required">The password is required</span>
+                        <span class="md-error" v-else-if="!$v.form.password.password">Invalid password</span>
+                        <span class="md-error" v-else-if="!$v.form.password.minLength">Password too short</span>
+                        <span class="md-error" v-else-if="!$v.form.password.maxLength">Password too long</span>
                     </md-field>
                 </md-card-content>
                 <md-card-actions>
@@ -33,15 +39,32 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import { validationMixin } from 'vuelidate'
+import { required, email, minLength, maxLength } from 'vuelidate/lib/validators'
 
 export default {
     middleware: 'auth',
+    mixins: [validationMixin], 
     data: () => ({
         form: {
             email: '',
             password: ''
         }
     }),
+    validations: {
+        form: {
+            email: {
+                required,
+                email
+            },
+            password: {
+                required,
+                minLength: minLength(6),
+                maxLength: maxLength(20)
+            }
+        },
+
+    },
     computed: {
         ...mapGetters(['loading', 'isAuthenticated'])
     },
@@ -53,6 +76,20 @@ export default {
         }
     },
     methods: {
+        validateForm() {
+            this.$v.$touch();
+            if (!this.$v.$invalid) {
+                this.registerUser()
+            }
+        },
+        getValidationClass(fieldName) {
+            const field = this.$v.form[fieldName];
+            if(field) {
+                return {
+                    "md-invalid": field.$invalid && field.$dirty
+                }
+            }
+        },
         async registerUser() {
             await this.$store.dispatch('authenticateUser', {
                 email: this.form.email,
